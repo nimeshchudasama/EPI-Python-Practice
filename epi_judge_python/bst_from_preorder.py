0b233dd19af750919def9c964e989858ff180b74
Nimesh Chudasama <nimechudasama@gmail.com>
2021-05-13 20:38:14 -0500

    Removed bst_from_preorder solution

--- epi_judge_python/bst_from_preorder.py
+++ epi_judge_python/bst_from_preorder.py
@@ -6,18 +6,8 @@ from test_framework import generic_test
 
 def rebuild_bst_from_preorder(preorder_sequence: List[int]
                               ) -> Optional[BstNode]:
-    if not preorder_sequence:
-        return None
-
-    transition_point = next(
-        (i
-         for i, a in enumerate(preorder_sequence) if a > preorder_sequence[0]),
-        len(preorder_sequence))
-    return BstNode(
-        preorder_sequence[0],
-        rebuild_bst_from_preorder(preorder_sequence[1:transition_point]),
-        rebuild_bst_from_preorder(preorder_sequence[transition_point:]))
-
+    # TODO - you fill in here.
+    return None
 
 
 if __name__ == '__main__':
